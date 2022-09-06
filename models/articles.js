b2f8728deb3d450cd73f29552b24c6759a9d08e2
Nedas Stockus <nedasstockus@gmail.com>
2022-09-06 11:27:39 +0100

    /api/articles/:article_id comment_count Model

--- models/articles.js
+++ models/articles.js
@@ -4,8 +4,12 @@ const { returnFirst } = require('../util/database')
 exports.fetchArticleByID = (id) => {
     return DB
         .query(`
-        SELECT * FROM articles
-        WHERE article_id = $1
+        SELECT  articles.*, 
+                CAST(COUNT(comments.comment_id) as INT) as comment_count
+        FROM articles
+        INNER JOIN comments ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id
         `, [id])
         .then(returnFirst)
 }
